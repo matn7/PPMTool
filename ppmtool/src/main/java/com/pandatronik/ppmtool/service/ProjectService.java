7877eb36a1a17b18f30d679cfcb4653c4579fe92
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2018-10-30 19:13:53 +0100

    CRUD completed

---
+++ ppmtool/src/main/java/com/pandatronik/ppmtool/service/ProjectService.java
@@ -0,0 +1,45 @@
+package com.pandatronik.ppmtool.service;
+
+import com.pandatronik.ppmtool.domain.Project;
+import com.pandatronik.ppmtool.exceptions.ProjectIdException;
+import com.pandatronik.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
+
+    public Project findProjectByIdentidier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exists");
+        }
+        return project ;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project with id: '" + projectId + "' does not exists");
+        }
+        projectRepository.delete(project);
+    }
+
+}
